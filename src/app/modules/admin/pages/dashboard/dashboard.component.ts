ae60385f833b666fef516a8619a488cb7dd46105
Mohamed Machlou <medmachlou@gmail.com>
2025-08-03 23:28:08 +0100

    nav bar component

--- src/app/modules/admin/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,10 +11,31 @@ export class DashboardComponent {
   // Injection
   router = inject(Router);
 
+  // 
+  openMenuStatus = signal<boolean>(true)
   
+  isScrolled = signal<boolean>(false)
+  
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled.set(window.scrollY > 50);
+    if(this.isScrolled()){
+      console.log('ok ok ok')
+    }
+  }
 
 
+// Switch Open/Close Menu
+switchMenu() {
 
+  this.openMenuStatus.set(!this.openMenuStatus())
+  console.log('switch status to : ', this.openMenuStatus())
+
+}
+
+closeMenu() {
+  this.openMenuStatus.set(false)
+}
 
   // Go To Link 
   goToLink(link: string): void {
